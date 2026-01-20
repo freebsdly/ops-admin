53e006f23faca85acd7adf99284a859c342cb085
qinhuajun <qinhuajun@outlook.com>
2026-01-20 22:13:21 +0800

    添加icon库，修改collapse按钮

--- src/app/layout/collapse-button/collapse-button.ts
+++ src/app/layout/collapse-button/collapse-button.ts
@@ -1,12 +1,17 @@
-import { ChangeDetectionStrategy, Component, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { tablerArrowNarrowLeftDashed, tablerArrowNarrowRightDashed } from '@ng-icons/tabler-icons';
+
 
 @Component({
   selector: 'app-collapse-button',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './collapse-button.html',
   styleUrl: './collapse-button.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  viewProviders: [provideIcons({ tablerArrowNarrowLeftDashed, tablerArrowNarrowRightDashed })]
 })
 export class CollapseButton {
+  readonly isCollapsed = input.required<boolean>();
   readonly toggle = output<void>();
 }
