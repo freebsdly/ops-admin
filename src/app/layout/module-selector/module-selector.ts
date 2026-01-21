b93361e846f02a0570856cb78328200ced962900
itoutsource.cz1731 <itoutsource.cz1731@trinasolar.com>
2026-01-21 11:49:13 +0800

    更新module selector

--- src/app/layout/module-selector/module-selector.ts
+++ src/app/layout/module-selector/module-selector.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { tablerLayoutGrid, tablerChevronDown } from '@ng-icons/tabler-icons';
 
@@ -10,5 +10,19 @@ import { tablerLayoutGrid, tablerChevronDown } from '@ng-icons/tabler-icons';
   viewProviders: [provideIcons({ tablerLayoutGrid, tablerChevronDown })]
 })
 export class ModuleSelector {
+  readonly modules = [
+    '模块 1',
+    '模块 2',
+    '模块 3',
+    '模块 4',
+    '模块 5',
+    '模块 6'
+  ];
 
+  readonly selectedModule = signal(this.modules[0]);
+  readonly selectedModuleText = computed(() => this.selectedModule());
+
+  selectModule(module: string): void {
+    this.selectedModule.set(module);
+  }
 }
