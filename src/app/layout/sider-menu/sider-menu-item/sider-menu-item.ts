ff8cbc6e25e098e18e92ffb8357639a2b059aad9
itoutsource.cz1731 <itoutsource.cz1731@trinasolar.com>
2026-01-21 15:06:15 +0800

    新增sider menu的popup menu

--- src/app/layout/sider-menu/sider-menu-item/sider-menu-item.ts
+++ src/app/layout/sider-menu/sider-menu-item/sider-menu-item.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, signal, ElementRef, viewChild, inject, AfterViewInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MenuItem } from '../sider-menu';
 
@@ -8,7 +8,10 @@ import { MenuItem } from '../sider-menu';
   templateUrl: './sider-menu-item.html',
   styleUrl: './sider-menu-item.css',
 })
-export class SiderMenuItem {
+export class SiderMenuItem implements AfterViewInit {
+  readonly elementRef = inject(ElementRef);
+  readonly menuLink = viewChild<ElementRef<HTMLDivElement>>('menuLink');
+
   readonly item = input.required<MenuItem>();
   readonly level = input(1);
   readonly maxLevel = input(3);
@@ -16,6 +19,24 @@ export class SiderMenuItem {
   readonly isCollapsed = input.required<boolean>();
   readonly toggleExpand = output<string>();
   readonly menuSelect = output<MenuItem>();
+
+  readonly showPopup = signal(false);
+  readonly popupPosition = signal({ top: 0, left: 0 });
+
+  ngAfterViewInit(): void {
+    this.updatePopupPosition();
+  }
+
+  updatePopupPosition(): void {
+    const linkElement = this.menuLink()?.nativeElement;
+    if (linkElement) {
+      const rect = linkElement.getBoundingClientRect();
+      this.popupPosition.set({
+        top: rect.top,
+        left: rect.right,
+      });
+    }
+  }
 
   hasChildren(): boolean {
     return Boolean(this.item().children && this.item().children!.length > 0);
@@ -26,14 +47,36 @@ export class SiderMenuItem {
   }
 
   onClickExpand(event: MouseEvent): void {
-    event.preventDefault();
-    event.stopPropagation();
-    this.toggleExpand.emit(this.item().path || this.item().label);
+    if (!this.isCollapsed()) {
+      event.preventDefault();
+      event.stopPropagation();
+      this.toggleExpand.emit(this.item().path || this.item().label);
+    }
+  }
+
+  onMouseEnter(): void {
+    if (this.isCollapsed() && this.hasChildren()) {
+      this.updatePopupPosition();
+      this.showPopup.set(true);
+    }
+  }
+
+  onMouseLeave(): void {
+    this.showPopup.set(false);
   }
 
   onClickMenuItem(event: MouseEvent): void {
     if (!this.hasChildren() && this.item().path) {
       this.menuSelect.emit(this.item());
+    }
+  }
+
+  onPopupItemClick(childItem: MenuItem, event: MouseEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    if (childItem.path) {
+      this.showPopup.set(false);
+      this.menuSelect.emit(childItem);
     }
   }
 }
